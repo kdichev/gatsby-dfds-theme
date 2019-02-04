2de2bdcc828b6d2451c3c38d6977875b72eaa9bb
Konstantin Dichev <konstantin.dichev@gmail.com>
2019-02-04 13:18:13 +0100

    feat(chore): add dfds site meta data

--- example/gatsby-config.js
+++ example/gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Gatsby DFDS Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@dfds`,
   },
-  plugins: [],
   __experimentalThemes: [
     {
       resolve: require.resolve('../'),
       options: { root: __dirname },
     },
   ],
+  plugins: [],
 }
